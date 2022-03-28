43e8d207c2b6451bd7c447444bfc95ca9c402fc4
Alex Pereira <alexapereira14@gmail.com>
2022-03-28 09:31:16 -0400

    feat: add method to push to word vector, and method to print

--- src/lib/data_types/word_vector/word_vector.h
+++ src/lib/data_types/word_vector/word_vector.h
@@ -9,4 +9,10 @@ WORD_VECTOR word_vector_init_default(void);
 
 void word_vector_free_from_memory(WORD_VECTOR * ptrToHWordVector);
 
+// ARRAY METHODS
+void word_vector_push(WORD_VECTOR hWordVector, WORD hWord);
+
+// UTILS
+void print_word_vector(WORD_VECTOR hWordVector);
+
 #endif
--- src/main.c
+++ src/main.c
@@ -3,6 +3,13 @@
 
 int main(int argc, char ** argv) {
   WORD_VECTOR myWordVector = word_vector_init_default();
+
+  WORD myWord = word_init_default();
+
+  word_vector_push(myWordVector, myWord);
+
+  print_word_vector(myWordVector);
+
   word_vector_free_from_memory(&myWordVector);
 
   return 0;
