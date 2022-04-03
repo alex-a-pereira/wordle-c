9fa43b64cada4d291ec81d1106d4021e17258c1a
Alex Pereira <alexapereira14@gmail.com>
2022-04-03 10:42:06 -0400

    chore: remove a comment

--- src/lib/data_types/word/word.h
+++ src/lib/data_types/word/word.h
@@ -4,14 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Word struct is only used by implementation so it's defined at top of word.c
-// It's added in comments here for convenience of those working on word implementation files
-// typedef struct _word {
-//   int length;
-//   int capacity;
-//   char * data;
-// } Word;
-
 // all consumers interact with a void ptr to set up strong boundaries between modules
 typedef void* WORD;
 
